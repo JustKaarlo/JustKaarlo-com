426c72ec36610dce47fb18e0d3577dc23e047309
Kaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-09-06 03:05:26 +0200

    Add files via upload

---
+++ js/indexFileStructure.js
@@ -0,0 +1,161 @@
+const fileStructure = [
+    {
+        type: "file",
+        name: "index.html",
+        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/index.html",
+        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/index.html",
+        size: 0,
+        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+        highlighted: true,
+        showOpenButton: true,
+        showRawButton: true,
+        showSizeTag: false
+    },
+    {
+        type: "file",
+        name: "Home.html",
+        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Home.html",
+        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Home.html",
+        size: 0,
+        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+        highlighted: true,
+        showOpenButton: true,
+        showRawButton: true,
+        showSizeTag: false
+    },
+    {
+        type: "file",
+        name: "Mods.html",
+        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Mods.html",
+        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Mods.html",
+        size: 0,
+        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+        highlighted: true,
+        showOpenButton: true,
+        showRawButton: true,
+        showSizeTag: false
+    },
+    {
+        type: "file",
+        name: "Reports.html",
+        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Reports.html",
+        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Reports.html",
+        size: 0,
+        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+        highlighted: true,
+        showOpenButton: true,
+        showRawButton: true,
+        showSizeTag: false
+    },
+    {
+        type: "folder",
+        name: "Embeds",
+        icon: "https://www.adenin.com/assets/logos/iframe.svg",
+        openUrl: "https://github.com/JustKaarlo/JustKaarlo-Google-Site/tree/6edc424da73f00e154c1fc637375469d2f432bfe/html/Embeds",
+        rawUrl: "https://your-download-link.com/new-file",
+        highlighted: false,
+        showOpenButton: true,
+        showRawButton: false,
+        showSizeTag: false,
+        showCountTag: true,
+        files: [
+            {
+                type: "folder",
+                name: "Reports",
+                icon: "https://tobebright.com/wp-content/uploads/edd/2022/01/Charcoal.png",
+                openUrl: "https://github.com/JustKaarlo/JustKaarlo-Google-Site/tree/6edc424da73f00e154c1fc637375469d2f432bfe/html/Reports",
+                highlighted: false,
+                showOpenButton: true,
+                showRawButton: false,
+                showSizeTag: false,
+                showCountTag: true,
+                files: [
+                    {
+                        type: "file",
+                        name: "Arrest-Report.html",
+                        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Reports/Arrest-Report.html",
+                        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Reports/Arrest-Report.html",
+                        size: 0,
+                        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+                        highlighted: false,
+                        showOpenButton: true,
+                        showRawButton: true,
+                        showSizeTag: false
+                    },
+                    {
+                        type: "file",
+                        name: "Citation-Report.html",
+                        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Reports/Citation-Report.html",
+                        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Reports/Citation-Report.html",
+                        size: 0,
+                        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+                        highlighted: false,
+                        showOpenButton: true,
+                        showRawButton: true,
+                        showSizeTag: false
+                    },
+                    {
+                        type: "file",
+                        name: "Coroner-Report.html",
+                        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Reports/Coroner-Report.html",
+                        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Reports/Coroner-Report.html",
+                        size: 0,
+                        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+                        highlighted: false,
+                        showOpenButton: true,
+                        showRawButton: true,
+                        showSizeTag: false
+                    },
+                    {
+                        type: "file",
+                        name: "Fire-Incident-Report.html",
+                        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Reports/Fire-Incident-Report.html",
+                        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Reports/Fire-Incident-Report.html",
+                        size: 0,
+                        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+                        highlighted: false,
+                        showOpenButton: true,
+                        showRawButton: true,
+                        showSizeTag: false
+                    },
+                    {
+                        type: "file",
+                        name: "Medical-Report.html",
+                        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Reports/Medical-Report.html",
+                        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Reports/Medical-Report.html",
+                        size: 0,
+                        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+                        highlighted: false,
+                        showOpenButton: true,
+                        showRawButton: true,
+                        showSizeTag: false
+                    },
+                    {
+                        type: "file",
+                        name: "Prison-Transport.html",
+                        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Reports/Prison-Transport.html",
+                        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Reports/Prison-Transport.html",
+                        size: 0,
+                        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+                        highlighted: false,
+                        showOpenButton: true,
+                        showRawButton: true,
+                        showSizeTag: false
+                    },
+                    {
+                        type: "file",
+                        name: "Towing-Report.html",
+                        openUrl: "https://justkaarlo.github.io/JustKaarlo-Google-Site/html/Reports/Towing-Report.html",
+                        rawUrl: "https://raw.githubusercontent.com/JustKaarlo/JustKaarlo-Google-Site/refs/heads/Home/html/Reports/Towing-Report.html",
+                        size: 0,
+                        icon: "https://cdn-icons-png.flaticon.com/256/11133/11133898.png",
+                        highlighted: false,
+                        showOpenButton: true,
+                        showRawButton: true,
+                        showSizeTag: false
+                    }
+                ]
+            }
+        ]
+    }
+];
