dcf143c0347170c4287e86722ba63972a80d359b
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-09-07 15:49:40 +0200

    Fixed convergence icon

--- Mods/js/Mod.Custom.Structure.js
+++ Mods/js/Mod.Custom.Structure.js
@@ -145,7 +145,7 @@
         container: erContainer,
         apiKey,
         customIcons: {
-            "Convergence (2.2.3).zip": "https://www.justkaarlo.www/Mods/src/filelist/Convergence.ico"
+            "Convergence (2.2.3).zip": "https://www.justkaarlo.com/Mods/src/filelist/Convergence.ico"
         },
         customFolderIcons: {},
         highlightFiles: [
