d51bb40de5cc7d953e0d78541f65ab0a2317df53
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-09-09 20:16:48 +0200

    Update Mods.FileList.Settings.js

--- js/Mods.FileList.Settings.js
+++ js/Mods.FileList.Settings.js
@@ -1,5 +1,4 @@
 (function initializeFileLists() {
-    // const apiKey = "AIzaSyDqtsRN35V1JnmfIY9hTsn_Ej4aSipsK4M";
     const apiKey = "__API_KEY__";
     const twRootFolderId = "10v2qU6Q01tZkyc7EuqGjz_YF_JA-AAiG";
     const twContainer = document.getElementById("tw-fileList");
