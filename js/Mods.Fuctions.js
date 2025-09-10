fb3282ed444d2be71822be77a5b966702f72894e
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-09-10 07:19:09 +0200

    update Mods.Fuctions.js

--- js/Mods.Fuctions.js
+++ js/Mods.Fuctions.js
@@ -210,7 +210,7 @@ function isDriveDoc(mimeType) {
 
 function getDownloadUrl(file, apiKey) {
     if (file.webContentLink) return file.webContentLink;
-    return 'http://app.justkaarlo.com/${file.id}'
+    return `http://app.justkaarlo.com/${file.id}`;
     // return `https://www.googleapis.com/drive/v3/files/${file.id}?alt=media&key=${apiKey}`;
 }
 
