d59884028b93287188d82ca1ac7e99dac2c6330d
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-09-17 17:09:07 +0200

    update fs25.settings.js

--- mods/js/fs25.settings.js
+++ mods/js/fs25.settings.js
@@ -12,9 +12,7 @@
         folderId: fsRootFolderId,
         container: fsContainer,
         apiKey,
-        customIcons: {
-            "Ground Textures Pack.zip": "../res/src/fs25-textures.png",
-        },
+        customIcons: {},
         customFolderIcons: {
             "1NQJdjAjXB8c5We33KuXdPltBGM6qQgpT": "https://cdn.buymeacoffee.com/uploads/membership_level/2024/06/x3t3ecJIeddW9EwyTs8ZbPV3KsmZTicsiIbWq4l8.png@800w_0e.webp",
             "1L8VdtjPgoVkN-LQeejAnvixvwfH0RsmQ": "../res/src/fs25-logo-small.png",
@@ -31,7 +29,6 @@
         excludeDownloadFiles: [],
         customDownloadLinks: {},
         customWebsiteLinks: {
-            "Ground Textures Pack.zip": "https://www.kingmods.net/en/fs25/mods/66301/ground-textures-pack",
             "1NQJdjAjXB8c5We33KuXdPltBGM6qQgpT": "https://www.kingmods.net/en/profile/fs-miner",
             "1L8VdtjPgoVkN-LQeejAnvixvwfH0RsmQ": "https://www.kingmods.net/en/fs25/new-mods",
             "1POXftu49rQW6coDWMVnpaQbLu7PrWEXs": "https://www.kingmods.net/en/fs25/categories/maps",
