0a9d29340f88ad75e745a94fc03eb275fd362ab7
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-09-23 14:26:10 +0200

    update fs25.settings.js

--- mods/js/fs25.settings.js
+++ mods/js/fs25.settings.js
@@ -13,7 +13,8 @@
         container: fsContainer,
         apiKey,
         customIcons: {
-            "FS25_rootCropStorage.zip": "https://www.justkaarlo.com/res/src/fs.ico"
+            "FS25_rootCropStorage.zip": "https://www.justkaarlo.com/res/src/fs.ico",
+            "FS25_EnhancedAnimalSystem.zip": "https://www.justkaarlo.com/res/src/fs.ico",
         },
         customFolderIcons: {
             "1NQJdjAjXB8c5We33KuXdPltBGM6qQgpT": "https://cdn.buymeacoffee.com/uploads/membership_level/2024/06/x3t3ecJIeddW9EwyTs8ZbPV3KsmZTicsiIbWq4l8.png@800w_0e.webp",
@@ -28,7 +29,8 @@
         ],
         folderGradientFallback: {},
         downloadButtonFiles: [
-            "FS25_rootCropStorage.zip"
+            "FS25_rootCropStorage.zip",
+            "FS25_EnhancedAnimalSystem.zip",
         ],
         excludeDownloadFiles: [],
         customDownloadLinks: {},
@@ -36,6 +38,8 @@
             "1NQJdjAjXB8c5We33KuXdPltBGM6qQgpT": "https://www.kingmods.net/en/profile/fs-miner",
             "1L8VdtjPgoVkN-LQeejAnvixvwfH0RsmQ": "https://www.kingmods.net/en/fs25/new-mods",
             "1POXftu49rQW6coDWMVnpaQbLu7PrWEXs": "https://www.kingmods.net/en/fs25/categories/maps",
+            "FS25_rootCropStorage.zip": "https://www.farming-simulator.com/mod.php?mod_id=303719&title=fs2025",
+            "FS25_EnhancedAnimalSystem.zip": "https://www.kingmods.net/en/fs25/mods/70701/enhanced-animal-system",
         },
         tooltipData: {
             "FS25_rootCropStorage.zip": {
@@ -48,6 +52,15 @@
                     "https://www.justkaarlo.com/res/fs25-screenshot/screenshot5.jpg"
                 ],
                 description: "This building is for storage of Potatoes, Beets, Carrots, Parsnips and Beetroots. This building functions like a Silo or Hayloft, but for root crops."
+            },
+            "FS25_EnhancedAnimalSystem.zip": {
+                images: [
+                    "https://www.kingmods.net/uploads/fs25/mods/enhanced-animal-system-fs25-XutyK.jpg",
+                    "https://www.kingmods.net/uploads/fs25/mods/enhanced-animal-system-fs25-Y3kkp.jpg",
+                    "https://www.kingmods.net/uploads/fs25/mods/enhanced-animal-system-fs25-z7YH2.jpg",
+                    "https://www.kingmods.net/uploads/fs25/mods/enhanced-animal-system-fs25-TEV0m.jpg",
+                ],
+                description: "This mod adjusts the animal system and expands it."
             },
         },
         showDownloadButtonAtRoot: true
