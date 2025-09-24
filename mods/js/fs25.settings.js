e12bac7c71042c581d21ffd15b633393862fbbac
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-09-24 17:59:13 +0200

    update fs25.settings.js

--- mods/js/fs25.settings.js
+++ mods/js/fs25.settings.js
@@ -1,5 +1,5 @@
 (function initializeFS25FileList() {
-    const apiKey = "__API_KEY__";
+    const apiKey = "AIzaSyDqtsRN35V1JnmfIY9hTsn_Ej4aSipsK4M";
     const fsRootFolderId = "1EqzEAoB3zUuDiAfA81DNmqQh97mH8ISR";
     const fsContainer = document.getElementById("fs-fileList");
     
@@ -40,10 +40,8 @@
             "FS25_Piccin_MODBT11.zip": "https://www.kingmods.net/en/fs25/mods/69804/piccin-advanced-mod-bt-11",
             "FS25_n0tLizardNeoPack.zip": "https://www.kingmods.net/en/fs25/mods/65192/lizard-neo-pack",
             "FS25_Bednar_Terraland.zip": "https://www.kingmods.net/en/fs25/mods/68816/bednar-terraland",
-            "FS25_Scorpio710.zip": "https://www.kingmods.net/en/fs25/mods/59104/elho-scorpio-710-stone-picker",
             "FS25_DeutzS9_AgrarTec.zip": "https://www.kingmods.net/en/fs25/mods/59330/deutz-fahr-sieries-9-agrar-tec",
             "FS25_JohnDeereGator6x4.zip": "https://www.kingmods.net/en/fs25/mods/70597/john-deere-gator-th-6x4",
-            "FS25_SWIFTER_SM_18000_By_TaZ_Modding.zip": "https://www.kingmods.net/en/fs25/mods/69635/bednar-swifter-sm18000",
             "FS25_JohnDeere_Ripper2100.zip": "https://www.kingmods.net/en/fs25/mods/69950/john-deere-2100-ripper",
             "FS25_CaseEcoloTil2500.zip": "https://www.kingmods.net/en/fs25/mods/69710/case-ecolo-til-2500",
         },
@@ -108,14 +106,6 @@
                 ],
                 description: ""
             },
-            "FS25_Scorpio710.zip": {
-                images: [
-                    "https://www.kingmods.net/uploads/fs25/mods/elho-scorpio-710-stone-picker-fs25-5MQKw.jpg",
-                    "https://www.kingmods.net/uploads/fs25/mods/elho-scorpio-710-stone-picker-fs25-Yi4vR.jpg",
-                    "https://www.kingmods.net/uploads/fs25/mods/elho-scorpio-710-stone-picker-fs25-aNcvI.jpg",
-                ],
-                description: ""
-            },
             "FS25_DeutzS9_AgrarTec.zip": {
                 images: [
                     "https://www.kingmods.net/uploads/fs25/mods/deutz-fahr-sieries-9-agrar-tec-fs25-q07oC.jpg",
@@ -129,13 +119,6 @@
                     "https://www.kingmods.net/uploads/fs25/mods/john-deere-gator-th-6x4-fs25-DCBsK.jpg",
                     "https://www.kingmods.net/uploads/fs25/mods/john-deere-gator-th-6x4-fs25-WXysT.jpg",
                     "https://www.kingmods.net/uploads/fs25/mods/john-deere-gator-th-6x4-fs25-ORl13.jpg",
-                ],
-                description: ""
-            },
-            "FS25_SWIFTER_SM_18000_By_TaZ_Modding.zip": {
-                images: [
-                    "https://www.kingmods.net/uploads/fs25/mods/bednar-swifter-sm18000-fs25-bixCY.jpg",
-                    "https://www.kingmods.net/uploads/fs25/mods/bednar-swifter-sm18000-fs25-0XOcC.jpg",
                 ],
                 description: ""
             },
@@ -161,14 +144,25 @@
             "FS25_n0tLizardNeoPack.zip": "Lizard Neo Pack",
             "FS25_Piccin_MODBT11.zip": "Piccin Advanced Mod BT 11",
             "FS25_TowingChain.zip": "Towing Chain With Hook",
-            "FS25_Scorpio710.zip": "ELHO Scorpio 710 Stone Picker",
             "FS25_DeutzS9_AgrarTec.zip": "Deutz-Fahr Sieries 9 - Agrar-Tec",
             "FS25_JohnDeereGator6x4.zip": "John Deere Gator TH 6x4",
-            "FS25_SWIFTER_SM_18000_By_TaZ_Modding.zip": "Bednar Swifter SM18000",
             "FS25_JohnDeere_Ripper2100.zip": "John Deere 2100 Ripper",
             "FS25_CaseEcoloTil2500.zip": "Case Ecolo-Til 2500",
         },
         customTags: {
+            "FS25_Bednar_Terraland.zip": [ { text: "Equipment", color: "#539b0bff", opacity: 0.2, textOpacity: 0.65 } ],
+            "FS25_CaseEcoloTil2500.zip": [ { text: "Equipment", color: "#539b0bff", opacity: 0.2, textOpacity: 0.65 } ],
+            "FS25_JohnDeere_Ripper2100.zip": [ { text: "Equipment", color: "#539b0bff", opacity: 0.2, textOpacity: 0.65 } ],
+            "FS25_n0tLizardNeoPack.zip": [ { text: "Equipment", color: "#539b0bff", opacity: 0.2, textOpacity: 0.65 } ],
+            "FS25_Piccin_MODBT11.zip": [ { text: "Equipment", color: "#539b0bff", opacity: 0.2, textOpacity: 0.65 } ],
+
+            "FS25_DeutzS9_AgrarTec.zip": [ { text: "Vehicle", color: "#57a20cff", opacity: 0.2, textOpacity: 0.65 } ],
+            "FS25_JohnDeereGator6x4.zip": [ { text: "Vehicle", color: "#57a20cff", opacity: 0.2, textOpacity: 0.65 } ],
+
+            "FS25_TowingChain.zip": [ { text: "Utilities", color: "#7d9664ff", opacity: 0.2, textOpacity: 0.65 } ],
+
+            "FS25_dice.zip": [ { text: "Other", color: "#7d9664ff", opacity: 0.2, textOpacity: 0.65 } ],
+            
             // "1L8VdtjPgoVkN-LQeejAnvixvwfH0RsmQ": [
             //     { text: "Bright", color: "#FF0000", opacity: 0.8, textOpacity: 1.0 },
             //     { text: "Subtle", color: "#00FF00", opacity: 0.3, textOpacity: 0.6 },
