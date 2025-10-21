09eef92ead4ba740da619688cbcdd1722015a0e2
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-10-21 22:42:15 +0200

    update error.html and fs25.settings.js

--- mods/js/fs25.settings.js
+++ mods/js/fs25.settings.js
@@ -2,6 +2,16 @@
     const apiKey = "__API_KEY__";
     const fsRootFolderId = "1EqzEAoB3zUuDiAfA81DNmqQh97mH8ISR";
     const fsContainer = document.getElementById("fs-fileList");
+
+    // CUSTOM CONSTANTS
+    const Fs25NorthAmericanMaps = "../res/src/fs25-north-american-maps.png";
+    const Fs25EuropeanMaps = "../res/src/fs25-european-maps.png";
+    const Fs25NorthAmericanTag = { text: "North America", color: "#6c7d00ff", opacity: 0.5, textOpacity: 0.5 };
+    const Fs25EuropeTag = { text: "Europe", color: "#6c7d00ff", opacity: 0.5, textOpacity: 0.5 };
+    const Fs252xMaps = { text: "2x", color: "#7d9000", opacity: 0.5, textOpacity: 0.45 };
+    const Fs254xMaps = { text: "4x", color: "#7d9000", opacity: 0.5, textOpacity: 0.45 };
+    const Fs2516xMaps = { text: "16x", color: "#7d9000", opacity: 0.5, textOpacity: 0.45 };
+
     
     if (!fsContainer) {
         console.error("FS25 file list container not found");
@@ -13,9 +23,11 @@
         container: fsContainer,
         apiKey,
         customIcons: {
-            "Huron County 16X.zip": "../res/src/fs25-north-american-maps.png",
-            "Schwesing Bahnhof.zip": "../res/src/fs25-european-maps.png",
-            "Freche Dachskarte 4X.zip": "../res/src/fs25-european-maps.png",
+            // MAPS
+            "Huron County 16X.zip": Fs25NorthAmericanMaps,
+            "Schwesing Bahnhof.zip": Fs25EuropeanMaps,
+            "Freche Dachskarte 4X.zip": Fs25EuropeanMaps,
+            "Saxlingham.zip": Fs25EuropeanMaps,
         },
         customFolderIcons: {
             "1L8VdtjPgoVkN-LQeejAnvixvwfH0RsmQ": "../res/src/fs25-logo-small.png",
@@ -34,9 +46,11 @@
             "1L8VdtjPgoVkN-LQeejAnvixvwfH0RsmQ",
             "1POXftu49rQW6coDWMVnpaQbLu7PrWEXs",
 
+            // MAPS
             "Huron County 16X.zip",
             "Schwesing Bahnhof.zip",
             "Freche Dachskarte 4X.zip",
+            "Saxlingham.zip",
         ],
         customDownloadLinks: {},
         customWebsiteLinks: {
@@ -46,9 +60,11 @@
             "FS25_Ropa_BFE.zip": "https://www.kingmods.net/en/fs25/mods/62298/ropa-tiger-6s-big-field-edition",
             "FS25_Balers_Pack_Whit_Windrower.zip": "https://www.kingmods.net/en/fs25/mods/61837/pack-of-balers-with-windrower",
 
+            // MAPS
             "Huron County 16X.zip": "https://www.kingmods.net/en/fs25/mods/63643/huron-county-16x",
             "Schwesing Bahnhof.zip": "https://www.kingmods.net/en/fs25/mods/62944/schwesing-bahnhof",
             "Freche Dachskarte 4X.zip": "https://www.kingmods.net/en/fs25/mods/60427/freche-dachskarte-4x",
+            "Saxlingham.zip": "https://www.kingmods.net/en/fs25/mods/69094/saxlingham",
         },
         tooltipData: {
             "FS25_Ropa_BFE.zip": {
@@ -93,6 +109,21 @@
                     "https://www.kingmods.net/uploads/fs25/mods/freche-dachskarte-4x-fs25-jwxtA.jpg",
                 ]
             },
+            "Saxlingham.zip": {
+                images: [
+                    "https://www.kingmods.net/uploads/fs25/mods/saxlingham-fs25-QSj04.jpg",
+                    "https://www.kingmods.net/uploads/fs25/mods/saxlingham-fs25-vzlbW.jpg",
+                    "https://www.kingmods.net/uploads/fs25/mods/saxlingham-fs25-j8Jj2.jpg",
+                ]
+            },
+
+            // "": {
+            //     images: [
+            //         "",
+            //         "",
+            //         "",
+            //     ]
+            // },
         },
         showDownloadButtonAtRoot: true,
         customDisplayNames: {
@@ -103,6 +134,7 @@
             "Huron County 16X.zip": "Huron County",
             "Schwesing Bahnhof.zip": "Schwesing Bahnhof",
             "Freche Dachskarte 4X.zip": "Freche Dachskarte",
+            "Saxlingham.zip": "Saxlingham",
         },
         customTags: {
             // "FILE/FOLDER NAME/ID": [
@@ -113,23 +145,29 @@
 
             // MAPS
             "Huron County 16X.zip": [
-                { text: "North America", color: "#6c7d00ff", opacity: 0.5, textOpacity: 0.5 },
-                { text: "16x", color: "#7d9000", opacity: 0.5, textOpacity: 0.45 },
+                Fs25NorthAmericanTag,
+                Fs2516xMaps,
             ],
             "Schwesing Bahnhof.zip": [
-                { text: "Europe", color: "#6c7d00ff", opacity: 0.5, textOpacity: 0.5 },
-                { text: "4x", color: "#7d9000", opacity: 0.5, textOpacity: 0.45 },
+                Fs25EuropeTag,
+                Fs254xMaps,
             ],
             "Freche Dachskarte 4X.zip": [
-                { text: "Europe", color: "#6c7d00ff", opacity: 0.5, textOpacity: 0.5 },
-                { text: "4x", color: "#7d9000", opacity: 0.5, textOpacity: 0.45 },
+                Fs25EuropeTag,
+                Fs254xMaps,
             ],
+            "Saxlingham.zip": [
+                Fs25EuropeTag,
+                Fs252xMaps,
+            ]
         },
         hideAllSizeTags: false,
         hideSizeTagFiles: [
+            // MAPS
             "Huron County 16X.zip",
             "Schwesing Bahnhof.zip",
             "Freche Dachskarte 4X.zip",
+            "Saxlingham.zip",
         ],
         hideSizeTagFolders: [
             "1L8VdtjPgoVkN-LQeejAnvixvwfH0RsmQ",
