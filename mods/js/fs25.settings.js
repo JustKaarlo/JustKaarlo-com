5a5ff579326cb0059ed89c5404220eddca77cf3d
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2025-10-31 21:56:14 +0100

    update fs25.settings.js

--- mods/js/fs25.settings.js
+++ mods/js/fs25.settings.js
@@ -1,5 +1,5 @@
 (function initializeFS25FileList() {
-    const apiKey = "AIzaSyDqtsRN35V1JnmfIY9hTsn_Ej4aSipsK4M";
+    const apiKey = "__API_KEY__";
     const fsRootFolderId = "1EqzEAoB3zUuDiAfA81DNmqQh97mH8ISR";
     const fsContainer = document.getElementById("fs-fileList");
 
