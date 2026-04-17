e43ab6ca1a37c44ce1c864215ccc79985bd88831
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2026-04-17 04:51:55 +0200

    v1.2.4.01

--- mods/js/fs25.settings.js
+++ mods/js/fs25.settings.js
@@ -128,6 +128,7 @@
             "map-58": "https://www.kingmods.net/en/fs25/mods/75994/district-of-drensteinfurt",
             "map-59": "https://www.kingmods.net/en/fs25/mods/76898/along-the-kiel-canal",
             "map-60": "https://www.kingmods.net/en/fs25/mods/76662/solek",
+            "map-61": "https://www.kingmods.net/en/fs25/mods/74978/birgland",
 
             // TEXTURES
 
@@ -571,6 +572,13 @@
                     "https://www.kingmods.net/uploads/fs25/mods/solek-fs25-KJsP7.jpg",
                 ],
             },
+            "map-61": {
+                images: [
+                    "https://www.kingmods.net/uploads/fs25/mods/birgland-fs25-wDWz9.jpg",
+                    "https://www.kingmods.net/uploads/fs25/mods/birgland-fs25-xwFJl.jpg",
+                    "https://www.kingmods.net/uploads/fs25/mods/birgland-fs25-ETGqx.jpg",
+                ],
+            },
 
             // MODS
             "mod-1": {
@@ -696,7 +704,7 @@
                 "map-17",
             ],
             "top4": [
-                "map-7", "map-15","map-16", "map-13", "map-14", "map-21", "map-48", "map-54", "map-52", "map-59",
+                "map-7", "map-15","map-16", "map-13", "map-14", "map-21", "map-48", "map-54", "map-52", "map-59", "map-61",
             ],
 
             // MAPS
@@ -705,7 +713,7 @@
             ],
             "2x-map": [],
             "4x-map": [
-                "map-1", "map-2", "map-3", "map-4", "map-6", "map-7", "map-11", "map-8", "map-9", "map-12", "map-13", "map-14", "map-15", "map-16", "map-17", "map-18", "map-19", "map-20", "map-21", "map-22", "map-24", "map-25", "map-26", "map-27", "map-28", "map-29", "map-30", "map-31", "map-32", "map-33", "map-39", "map-41", "map-47", "map-49", "map-51", "map-53", "map-54", "map-57", "map-52", "map-58", "map-59", "map-60",
+                "map-1", "map-2", "map-3", "map-4", "map-6", "map-7", "map-11", "map-8", "map-9", "map-12", "map-13", "map-14", "map-15", "map-16", "map-17", "map-18", "map-19", "map-20", "map-21", "map-22", "map-24", "map-25", "map-26", "map-27", "map-28", "map-29", "map-30", "map-31", "map-32", "map-33", "map-39", "map-41", "map-47", "map-49", "map-51", "map-53", "map-54", "map-57", "map-52", "map-58", "map-59", "map-60", "map-61",
             ],
             "16x-map": [
                 "map-5",
@@ -1112,6 +1120,12 @@
             {
                 id: "map-60",
                 name: "Solek",
+                mimeType: "application/zip",
+                parentId: "fs25-maps"
+            },
+            {
+                id: "map-61",
+                name: "Birgland",
                 mimeType: "application/zip",
                 parentId: "fs25-maps"
             },
