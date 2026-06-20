c9d1731e6f1478c1465358dfb13d634c4b50e0c7
NotKaarlo <109157146+JustKaarlo@users.noreply.github.com>
2026-06-20 21:54:35 +0200

    v1.2.9.7

--- guides/settings.js
+++ guides/settings.js
@@ -2267,7 +2267,7 @@ const gzwGuides = [
         contentType: 'link',
         link: 'https://gzwloadout.com/vendors',
         badge: {
-            icon: 'https://www.justkaarlo.com/res/guides/grayzone/layout/buttons/badges/third-party/gzwloadout-com.png',
+            icon: 'https://www.justkaarlo.com/res/guides/grayzone/layout/buttons/icons/core/grayzone-yellow.png',
             position: 'right'
         }
     },
@@ -2278,7 +2278,7 @@ const gzwGuides = [
         contentType: 'link',
         link: 'https://gzwitems.com/',
         badge: {
-            icon: 'https://www.justkaarlo.com/res/guides/grayzone/layout/buttons/badges/third-party/gzwitems-com.png',
+            icon: 'https://www.justkaarlo.com/res/guides/grayzone/layout/buttons/icons/core/grayzone-yellow.png',
             position: 'right'
         }
     },
@@ -2289,7 +2289,18 @@ const gzwGuides = [
         contentType: 'link',
         link: 'https://gzwtacmap.com/maps/lamang',
         badge: {
-            icon: 'https://www.justkaarlo.com/res/guides/grayzone/layout/buttons/badges/third-party/gzwtacmap-com.png',
+            icon: 'https://www.justkaarlo.com/res/guides/grayzone/layout/buttons/icons/core/grayzone-yellow.png',
+            position: 'right'
+        }
+    },
+    {
+        title: 'Blueprints',
+        subtitle: 'WhereIsVulture.com',
+        icon: 'https://www.justkaarlo.com/res/guides/grayzone/layout/buttons/icons/core/blueprint.png',
+        contentType: 'link',
+        link: 'https://whereisvulture.com/blueprints',
+        badge: {
+            icon: 'https://www.justkaarlo.com/res/guides/grayzone/layout/buttons/icons/core/grayzone-yellow.png',
             position: 'right'
         }
     },
